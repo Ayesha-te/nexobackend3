911b5a9f67eea43b48d106e355493f75a224de85
Hamza Abdul Rauf <hharc123@gmail.com>
2026-05-10 12:25:12 +0500

    new1

--- api/_handler.ts
+++ api/_handler.ts
@@ -1,20 +1,20 @@
 import app, { ensureBackendReady } from "../src/server.js";
 
 function applyOpenCorsHeaders(req: any, res: any) {
-  const requestedHeaders = req?.headers?.["access-control-request-headers"];
-  const allowHeaders = Array.isArray(requestedHeaders)
-    ? requestedHeaders.join(",")
-    : requestedHeaders ||
-      "Origin, X-Requested-With, Content-Type, Accept, Authorization";
-
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS");
-  res.setHeader("Access-Control-Allow-Headers", allowHeaders);
+  const origin = req.headers.origin || "*";
+  
+  res.setHeader("Access-Control-Allow-Origin", origin);
+  res.setHeader("Access-Control-Allow-Methods", "GET, HEAD, PUT, PATCH, POST, DELETE, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  res.setHeader("Access-Control-Max-Age", "86400");
+  res.setHeader("Vary", "Origin");
 }
 
 export default async function handler(req: any, res: any) {
+  // Apply CORS headers to all responses
   applyOpenCorsHeaders(req, res);
 
+  // Handle preflight requests
   if (req.method === "OPTIONS") {
     res.statusCode = 204;
     res.end();
